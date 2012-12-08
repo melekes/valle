b8a5f0e112cdb1c32861988678e5b03e5b30411c
Andrew8xx8 <avk@8xx8.ru>
2012-12-08 13:16:23 +0400

    Test for getting string bounds added.

--- lib/valle/bound_mapper.rb
+++ lib/valle/bound_mapper.rb
@@ -1,11 +1,23 @@
 module Valle
+  Bound = Struct.new :minimum, :maximum
+
   class BoundMapper
-    
+
     def initialize
     end
 
-    def bound_for(column)
-      
+    def bound(column)
+      Bound.new minimum(column), maximum(column)
+    end
+
+    private
+
+    def minimum(column)
+
+    end
+
+    def maximum(column)
+      column.limit
     end
   end
 end
--- test/lib/bound_mapper_test.rb
+++ test/lib/bound_mapper_test.rb
@@ -3,12 +3,21 @@ require "test_helper"
 class BoundMapperTest < TestCase
 
   def setup
-    @column = ActiveRecord::ConnectionAdapters::Column.new("test_column", "")
     @bound_mapper = Valle::BoundMapper.new
   end
-  
-  def test_sould_return_bounds_for_column
-    assert @bound_mapper.bound_for(@column), "Bound for column '#{@column.name}' not found"
+
+  def test_should_return_bound_for_abstract_column
+    column = ActiveRecord::ConnectionAdapters::Column.new("test_column", "")
+    assert @bound_mapper.bound(column), "Bound for column '#{column.name}' not found"
+  end
+
+  def test_should_return_bound_for_column_with_type_string
+    column = ActiveRecord::ConnectionAdapters::Column.new("test_column", "", "string(255)")
+
+    bound = @bound_mapper.bound(column)
+
+    assert_equal nil, bound.minimum
+    assert_equal 255, bound.maximum
   end
 
 end
