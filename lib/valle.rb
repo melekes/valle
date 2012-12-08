8ad83133af71fdbd7b5016c9a06ca999632b34c3
Anton Kalyaev <anton.kalyaev@gmail.com>
2012-12-08 15:28:43 +0400

    updated autoloading

--- lib/valle.rb
+++ lib/valle.rb
@@ -1,6 +1,8 @@
-require "valle/version"
+require 'valle/macro_methods'
 require 'active_record'
 
 module Valle
+  autoload :BoundAssigner, 'valle/bound_assigner'
   autoload :BoundMapper, 'valle/bound_mapper'
+  autoload :ValidationSetter, 'valle/validation_setter'
 end
