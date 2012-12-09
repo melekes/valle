a1d320142b1b2379afd1e279d4f95dfa1c31f73d
Anton Kalyaev <anton.kalyaev@gmail.com>
2012-12-09 17:37:48 +0400

    fixed valle configuration

--- lib/valle/configuration.rb
+++ lib/valle/configuration.rb
@@ -1,10 +1,13 @@
+# TODO [AK 09/12/12] maybe we should use ActiveSupport::Configurable ?
+require 'active_support/core_ext/module/attribute_accessors'
+
 module Valle
   module Configuration
 
     mattr_accessor :options
 
     self.options = {
-      enabled: true # gem is enabled by default
+      enabled: true, # gem is enabled by default
       models: nil # nil means to select all the AR models
     }
 
