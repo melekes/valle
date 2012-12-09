304669e064ef69eae95b6fe58f48a394f1282efb
Anton Kalyaev <anton.kalyaev@gmail.com>
2012-12-09 22:47:23 +0400

    using engine if rails defined

--- lib/valle.rb
+++ lib/valle.rb
@@ -1,10 +1,27 @@
 require 'valle/configuration'
-require 'active_record'
 
 module Valle
   extend Configuration
 
+  # core
   autoload :BoundMapper, 'valle/bound_mapper'
   autoload :BoundsManager, 'valle/bounds_manager'
   autoload :ValidationSetter, 'valle/validation_setter'
+
+  # hooks
+  autoload :Hooks, 'valle/hooks'
+end
+
+# load Rails/Railtie
+begin
+  require 'rails'
+rescue LoadError
+  #do nothing
+end
+
+# if not using Railtie, call `Valle::Hooks.init` directly
+# TODO [AK 09/12/12] move this block to initializers
+if defined? Rails
+  require 'valle/railtie'
+  require 'valle/engine'
 end
