624ce1757fb34c7b9577a2fdb0efd9670dcfa956
Anton Kalyaev <anton.kalyaev@gmail.com>
2012-12-09 15:56:34 +0400

    refactored bound_mapper tests

--- test/lib/bound_mapper_test.rb
+++ test/lib/bound_mapper_test.rb
@@ -6,18 +6,15 @@ class BoundMapperTest < TestCase
     @bound_mapper = Valle::BoundMapper.new
   end
 
-  def test_should_return_bound_for_abstract_column
+  def test_should_return_bound
     column = ActiveRecord::ConnectionAdapters::Column.new("test_column", "")
     assert @bound_mapper.bound(column), "Bound for column '#{column.name}' not found"
   end
 
-  def test_should_return_bound_for_column_with_type_string
+  def test_should_return_maximum_for_column_with_type_string
     column = ActiveRecord::ConnectionAdapters::Column.new("test_column", "", "string(255)")
-
     bound = @bound_mapper.bound(column)
 
-    assert_equal nil, bound.minimum
     assert_equal 255, bound.maximum
   end
-
 end
