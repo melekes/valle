5298b131fa22bf89adec7da6269ec15b06d2dcfd
Anton Kalyaev <anton.kalyaev@gmail.com>
2012-12-09 17:30:25 +0400

    commented validation_setter

--- lib/valle/validation_setter.rb
+++ lib/valle/validation_setter.rb
@@ -2,7 +2,14 @@ module Valle
   class ValidationSetter
     class << self
 
-      def inject(bound, column, klass)
+      ##
+      # Adds validator to the klass column depending on its type
+      #
+      # @param [Bound] bound the bound
+      # @param [Column] column the column
+      # @param [ActiveRecord::Base] klass the AR model class
+      #
+      def add_validator(bound, column, klass)
         case column.type
         when :string
           klass.validates column.name, length: { maximum: bound.maximum }
