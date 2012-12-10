8e8e2ba334b6aff6f7545dfa6132e1ad6d2d6098
Anton Kalyaev <anton.kalyaev@gmail.com>
2012-12-10 22:21:57 +0400

    implemented min and max byte limited methods

--- lib/valle/abstract_adapter/byte_limited_column.rb
+++ lib/valle/abstract_adapter/byte_limited_column.rb
@@ -3,11 +3,23 @@ module Valle
     class ByteLimitedColumn < AbstractColumn
 
       def maximum
-        # TODO [AK] implement
+        case limit
+        when 1; 127
+        when 2; 32767
+        when 3; 8388607
+        when 4; 2147483647
+        when 8; 9223372036854775807
+        end
       end
 
       def minimum
-        # TODO [AK] implement
+        case limit
+        when 1; -128
+        when 2; -32768
+        when 3; -8388608
+        when 4; -2147483648
+        when 8; -9223372036854775808
+        end
       end
     end
   end
