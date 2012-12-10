2521bb847233b5329232949e704cb3fc741462e3
Anton Kalyaev <anton.kalyaev@gmail.com>
2012-12-10 22:22:48 +0400

    added validator for integer type

--- lib/valle/validation_setter.rb
+++ lib/valle/validation_setter.rb
@@ -9,9 +9,16 @@ module Valle
       # @param [ActiveRecord::Base] klass the AR model class
       #
       def add_validator(column, klass)
+        options = {}
+        options[:minimum] = column.minimum if column.minimum
+        options[:maximum] = column.maximum if column.maximum
+        return false unless options.present?
+
         case column.type
-        when :string
-          klass.validates column.name, length: { maximum: column.maximum }
+        when :string, :text
+          klass.validates column.name, length: options
+        when :integer
+          klass.validates column.name, numericality: options
         end
       end
     end
