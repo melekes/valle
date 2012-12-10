4540e10a388f7f8830afb1d42ebc49d65463d136
Kirillov Alexander <saratovsource@gmail.com>
2012-12-10 23:28:39 +0400

    change the call to override inheritance

--- lib/valle/hooks.rb
+++ lib/valle/hooks.rb
@@ -6,11 +6,16 @@ module Valle
       def init
         ActiveSupport.on_load(:active_record) do
           ActiveRecord::Base.class_eval do
-            # TODO [AK 09/12/12] possible we should run it after the class was inherited
-            # @see http://stackoverflow.com/q/7093992/820520
-            def self.inherited(subclass)
+
+            def self.inherited_with_valle_validators(subclass)
               Valle::BoundsManager.add_validators(subclass)
+              inherited_without_valle_validators(subclass)
             end
+
+            class << self
+              alias_method_chain :inherited, :valle_validators
+            end
+
           end
         end
       end
