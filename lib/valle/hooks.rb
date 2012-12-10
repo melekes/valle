f85bc6a06f92e8fb535e01e92aa0a2948805be8b
Kirillov Alexander <saratovsource@gmail.com>
2012-12-11 01:09:05 +0400

    move method into block

--- lib/valle/hooks.rb
+++ lib/valle/hooks.rb
@@ -7,12 +7,13 @@ module Valle
         ActiveSupport.on_load(:active_record) do
           ActiveRecord::Base.class_eval do
 
-            def self.inherited_with_valle_validators(subclass)
-              Valle::BoundsManager.add_validators(subclass)
-              inherited_without_valle_validators(subclass)
-            end
-
             class << self
+
+              def inherited_with_valle_validators(subclass)
+                Valle::BoundsManager.add_validators(subclass)
+                inherited_without_valle_validators(subclass)
+              end
+
               alias_method_chain :inherited, :valle_validators
             end
 
