28fd4006b3648047e5686303c13db142fffcc3a8
Anton Kalyaev <anton.kalyaev@gmail.com>
2013-11-08 18:26:57 +0400

    apply validations on subclassing

--- lib/valle/hooks.rb
+++ lib/valle/hooks.rb
@@ -39,18 +39,7 @@ module Valle
       # @note ActiveRecord::Validations should be defined at this point
       #
       def extend_ar_validations_valid_method(subclass)
-        subclass.instance_eval do
-          cattr_accessor :valle_validators
-        end
-
-        subclass.class_eval do
-          def valid_with_valle_validators?(context = nil)
-            self.class.valle_validators ||= Valle::Manager.add_validators(self.class)
-            valid_without_valle_validators?(context)
-          end
-
-          alias_method_chain :valid?, :valle_validators
-        end
+        Valle::Manager.add_validators(subclass)
       end
     end
   end
